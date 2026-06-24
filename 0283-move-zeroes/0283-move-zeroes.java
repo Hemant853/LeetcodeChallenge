c5a6bcd9a31571dd28ce29c7be57fda1a4ca9d69
Hemant Chauhan <128022384+Hemant853@users.noreply.github.com>
2026-06-24 21:39:58 +0530

    Time: 2 ms (91.99%), Space: 47.8 MB (37.7%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,13 @@
 class Solution {
     public void moveZeroes(int[] arr) {
-        int count=0, k=0;
-
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] == 0) {
-                count++;
-            } else {
-                arr[k] = arr[i];
-                k++;
-            }
-        }
-
-        for(int i=0; i<count; i++) {
-            arr[k] = 0;
-            k++;
-        }
+       int j=0;
+       for(int i=0; i<arr.length; i++) {
+         if(arr[i] != 0) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j++;
+         }
+       }
     }
 }
